63e15c4ae4f5733ea8efc4b589e14229f8fd09ae
dumitrw <nubitrwgt@gmailcom>
2025-06-18 21:57:52 +0300

    renamed:    badges/active_developer.png -> public/badges/active_developer.png 	renamed:    badges/booster.png -> public/badges/booster.png 	renamed:    badges/brilliance.png -> public/badges/brilliance.png 	renamed:    badges/legacy_user.png -> public/badges/legacy_user.png 	renamed:    badges/nitro.png -> public/badges/nitro.png 	renamed:    badges/quest_completed.png -> public/badges/quest_completed.png 	modified:   public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Versiunea cache-ului - modifică la fiecare deploy pentru a forța update
-const CACHE_NAME = 'dumitrw-cache-v2.3';
+const CACHE_NAME = 'dumitrw-cache-v2.4';
 
 // URLs care vor fi cache-uite la instalare
 const URLS_TO_CACHE = [
