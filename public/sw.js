fcbf1ae2ea9805b4a28c814a5cb4119687961adf
dumitrw <nubitrwgt@gmailcom>
2025-07-14 16:46:10 +0300

    modified:   public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Versiunea cache-ului - modifică la fiecare deploy pentru a forța update
-const CACHE_NAME = 'dumitrw-cache-v3.24 spotify/';
+const CACHE_NAME = 'dumitrw-cache-v4 dither/';
 
 // URLs care vor fi cache-uite la instalare
 const URLS_TO_CACHE = [
