a62ff2eff70b01a7b56ae511cabd4afc0959ebf1
dumitrw <nubitrwgt@gmailcom>
2025-07-31 14:31:29 +0300

    modified:   public/sw.js 	modified:   src/components/scripts/SplashCursorWrapper.jsx

--- src/components/scripts/SplashCursorWrapper.jsx
+++ src/components/scripts/SplashCursorWrapper.jsx
@@ -1,6 +1,6 @@
 // src/components/scripts/SplashCursorWrapper.jsx
 'use client';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import SplashCursor from './SplashCursor.jsx'; // Asigură-te că calea este corectă
 
 const SplashCursorWrapper = () => {
