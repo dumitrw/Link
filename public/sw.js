7c4363754a4d1a32efc27ed709a7a8925b405779
dumitrw <nubitrwgt@gmailcom>
2025-07-31 07:26:26 +0300

    modified:   public/sw.js 	modified:   src/components/css/DiscordPresence.css 	new file:   src/components/css/DiscordPresence.css.txt 	renamed:    src/components/css/glasss.css -> src/components/css/glasss.css.txt 	modified:   src/components/presence/DiscordPresence.jsx 	new file:   src/components/presence/DiscordPresence.jsx.txt 	renamed:    src/components/scripts/glasss.jsx -> src/components/scripts/glasss.jsx.txt

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Versiunea cache-ului - modifică la fiecare deploy pentru a forța update
-const CACHE_NAME = 'cache-v6.5 fix+redesign+stremio+ass';
+const CACHE_NAME = '(cache-v6.5 fix+redesign+stremio+ass) v6.4 rollback';
 
 // URLs care vor fi cache-uite la instalare
 const URLS_TO_CACHE = [
