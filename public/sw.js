c480e7bf60192d750c87e5194dd19b0f14f7ad3a
dumitrw <nubitrwgt@gmailcom>
2026-02-11 22:15:39 +0200

    modified:   public/sw.js 	modified:   src/layouts/GameLayout.astro 	modified:   src/pages/games/blackjack.astro

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Versiunea cache-ului - modifică la fiecare deploy pentru a forța update
-const CACHE_NAME = 'cache-v11.02.2026 2';
+const CACHE_NAME = 'cache-v11.02.2026 3';
 
 // URLs care vor fi cache-uite la instalare
 const URLS_TO_CACHE = [
